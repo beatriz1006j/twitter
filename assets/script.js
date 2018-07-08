1fdcaeafcf8e6068eda3084eb84e60df9757c67c
beatriz1006j <beatriz10j@gmail.com>
2018-07-08 19:23:18 -0300

    atualizar

---
+++ assets/script.js
@@ -0,0 +1,44 @@
+ function limite_total(valor) {
+    var inicial = 140;
+    var total = valor.length;
+    if(total <= inicial) {
+        var resto = inicial - total;
+        document.getElementById('contador').innerHTML = resto;
+    } 
+	if (resto >=31) {
+    document.querySelector("#contador").style.color = "black";
+  }
+	if (resto <= 30) {
+    document.querySelector("#contador").style.color = "red";
+  }
+	if (resto <= 10) {
+    document.querySelector("#contador").style.color = "yellow";
+  }
+	
+	
+    
+}
+
+function enviar(){
+  var saida = document.getElementById("msg").value;
+  document.getElementById('saida').innerHTML += "<p>" + saida + "</p>";
+  document.getElementById("msg").value = "";
+    
+    
+    
+    if(saida ===""){
+        alert("É necessariio digitar uma mensagem");
+        
+    }
+  
+
+}
+
+function desabit_Botao(){
+    
+    if(valor.length === ""){
+     document.getElementById('button').disabled = true;
+} 
+  
+
+}
